0844b3240ebeb582cd826ca597299a29b6b4d29c
Dioonel <dionelmartinezc@gmail.com>
2022-12-27 18:40:33 -0300

    Basic keyboard layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'keyboard';
+export class AppComponent implements OnInit{
+  constructor(){}
+
+  ngOnInit() {
+    let container = document.getElementById('container');
+    container?.focus();
+  }
+
+  tap(e: Event){
+    let event = e as KeyboardEvent;
+    console.log(event.code);
+    let element = document.getElementById(event.code);
+    if(element){
+      element.classList.add('active');
+    }
+  }
+
+  untap(e: Event){
+    let event = e as KeyboardEvent;
+    console.log(event.code);
+    let element = document.getElementById(event.code);
+    if(element){
+      element.classList.remove('active');
+    }
+  }
 }
+

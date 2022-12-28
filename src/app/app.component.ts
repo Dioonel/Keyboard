e0fd27e1b9d9c7ce67799583c71e084e40c6edee
Dioonel <dionelmartinezc@gmail.com>
2022-12-28 18:08:45 -0300

    Added keys per second counter and fixed bugs
    Fixed Tab and AltLeft making the keyboard crash.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { LowerCasePipe } from '@angular/common';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  constructor(){}
+  kps = 0;
+
+  constructor(){ }
 
   ngOnInit() {
     let container = document.getElementById('container');
@@ -15,16 +18,23 @@ export class AppComponent implements OnInit{
 
   tap(e: Event){
     let event = e as KeyboardEvent;
-    console.log(event.code);
+    if (event.repeat) { return }
+    if (event.code === 'Tab') e.preventDefault()
+    else if (event.code === 'AltLeft')  e.preventDefault()
+
     let element = document.getElementById(event.code);
     if(element){
+      this.kps++;
+      setTimeout(() => { this.kps-- }, 1000);
       element.classList.add('active');
     }
   }
 
   untap(e: Event){
     let event = e as KeyboardEvent;
-    console.log(event.code);
+    if (event.code === 'Tab') e.preventDefault()
+    else if (event.code === 'AltLeft')  e.preventDefault()
+
     let element = document.getElementById(event.code);
     if(element){
       element.classList.remove('active');
